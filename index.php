60f789ed9ee38ecf02739abc5b22406b2d5074e6
Craig Nguyen <craigcosmo@gmail.com>
2017-09-02 13:12:51 +0700

    update

--- index.php
+++ index.php
@@ -3,33 +3,42 @@ function readcsv($filename){
 	$file = fopen($filename, 'r');
 	while (($line = fgetcsv($file)) !== FALSE) {
 	  //$line is an array of the csv elements
-	  print_r($line);
+		// print_r($line);
+		insertdata($sline)
 	}
 	fclose($file);
 }
 
 
 
-$host ="localhost";
-$username = "root";
-$password = "root";
-$database = "kingsmen";
-$table ="marketdata";
-$con = mysql_connect($host,$username,$password);
+function insertdata($data){
 
-// echo "hello";
+	$host ="localhost";
+	$username = "root";
+	$password = "root";
+	$database = "kingsmen";
+	$table ="marketdata";
 
-if($con === true){
-   echo "connected";
+	$sql = "INSERT INTO marketdata (isin, name, `date`, price, volume, status)
+	VALUES ($data[0], $data[1], $data[2], $data[3], $data[4], $data[5])";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}else{
+		// echo "sql connected";
+	}
+
+	if (mysqli_query($conn, $sql)) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+
 }
 
-if($con === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-// function insertdata(){
-
-// }
+insertdata( [1232, 'lego', '2017-06-11'], 65, 1, 0)
 
 // readcsv('file.csv')
 
